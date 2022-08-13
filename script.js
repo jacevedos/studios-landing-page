2d56040a011497aee9f43b3a5d7817460c079014
jacevedos <jacevedos@unbosque.edu.co>
2022-08-13 11:22:07 -0500

    add all the elements

---
+++ script.js
@@ -0,0 +1,16 @@
+((d)=>{
+    const $btnMenu=d.querySelector(".menu"),
+    $menu=d.querySelector(".options");
+
+    $btnMenu.addEventListener("click",e=>{
+        $btnMenu.firstElementChild.classList.toggle("none")
+        $btnMenu.lastElementChild.classList.toggle("none")
+        $menu.classList.toggle("is-active");
+    });
+    d.addEventListener("click",e=>{
+        if(!e.target.matches(".options a")) return false;
+        $btnMenu.firstElementChild.classList.remove("none")
+        $btnMenu.lastElementChild.classList.add("none")
+        $menu.classList.remove("is-active");
+    })
+})(document);
